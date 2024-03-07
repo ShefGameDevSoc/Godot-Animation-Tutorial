4b068a835e8c8b997e06a947689b46b2150a7ab6
Ayyub Ameen <ayuubumar1234@gmail.com>
2024-03-07 17:24:32 +0000

    Added assets

---
+++ scripts/Player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+@export var health = 10
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var anim = get_node("AnimationPlayer")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		#anim.play("Jump")
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	#if direction == -1:
+		#get_node("AnimatedSprite2D").flip_h = true
+	#elif direction == 1:
+		#get_node("AnimatedSprite2D").flip_h = false
+		
+	if direction:
+		velocity.x = direction * SPEED
+		#if velocity.y == 0:
+			#anim.play("Run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#if velocity.y == 0:
+			#anim.play("Idle")
+			
+	#if velocity.y > 0:
+		#anim.play("Fall")
+
+	move_and_slide()
+	
+	if health <= 0:
+		queue_free()
+		get_tree().change_scene_to_file("res://menus/main menu/Main.tscn")
